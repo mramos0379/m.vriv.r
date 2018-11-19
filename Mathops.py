f5a18ccbde8af585cdc83a6ca8c3fcdc3eaffaf1
mramos0379 <44813865+mramos0379@users.noreply.github.com>
2018-11-19 10:27:27 -0700

    Add files via upload

---
+++ Mathops.py
@@ -0,0 +1,27 @@
+'''
+math ops and operands
+'''
+
+a = 25
+b = 10
+c = 7
+
+print("print the result of : ",a, "+",b, "=" ,a+b)
+print("print the result of : ",a, "-",b, "=" ,a-b)
+print("print the result of : ",a, "*",b, "=" ,a*b)
+print("print the result of : ",a, "/",b, "=" ,a/b)
+
+# Integer Division //
+
+print("print the result of : ",a, "//",b, "=" ,a//b)
+print("print the result of : ",a, "//",c, "=" ,a//c)
+
+# modulus ( remainder ) of an Integer Division
+
+print("print the result of : ",a, "%",b, "=" ,a%b)
+
+print("print the result of : ",c, "$",b, "=" ,c%b)
+
+# Power of a base raised to an exponent
+print(" Find 2 raised at 5th power  :", 2**5)
+
